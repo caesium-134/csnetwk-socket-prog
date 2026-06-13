8a6e3e32389a3ba21f077efbefa310d2de71362c
caesium-134 <nina_abogadie@dlsu.edu.ph>
2026-06-13 16:24:41 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,75 @@
+import socket
+import json
+
+HOST = socket.gethostname()
+PORT = 5555
+
+CLIENT_NAME = "Client of _____" # fill in later 
+
+def get_number():
+    
+    while True:
+        try:
+            num = int(
+                input("Enter a number (1-100): ")
+            )
+
+            if 1 <= num <= 100:
+                return num
+
+            print("Number must be between 1 and 100.")
+
+        except ValueError:
+            print("Invalid input. Enter an integer.")
+
+
+def main():
+
+    number = get_number()
+
+    clientsocket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM
+    )
+
+    print("CLIENT: Connecting...")
+
+    clientsocket.connect((HOST, PORT))
+
+    print("CLIENT: Connected.")
+
+    message = {
+        "name": CLIENT_NAME,
+        "number": number
+    }
+
+    clientsocket.send(
+        json.dumps(message).encode()
+    )
+
+    print("CLIENT: Message sent.")
+
+    data = clientsocket.recv(1024).decode()
+
+    response = json.loads(data)
+
+    server_name = response["name"]
+    server_number = response["number"]
+
+    print("\nCLIENT: Reply received.")
+
+    print(f"Client Name: {CLIENT_NAME}")
+    print(f"Server Name: {server_name}")
+
+    print(f"Client Number: {number}")
+    print(f"Server Number: {server_number}")
+
+    print(f"Sum: {number + server_number}")
+
+    clientsocket.close()
+
+    print("CLIENT: Socket closed.")
+
+
+if __name__ == "__main__":
+    main()
